d105dc3f1c1eb5330737ca6126b8e7a0092a11b0
onik-999 <onikdas.dev@gmail.com>
2026-09-19 01:20:46 +0600

    Add solution for problem 04: Maximum Depth of Binary Tree

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -5,25 +5,22 @@
  * @param {number} k
  * @return {boolean}
  */
-var checkSubarraySum = function(nums, k) {
-    if (nums.length === 0) return 0;
-    
-    let i = 0;
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
-            i++;
-            nums[i] = nums[j];
-        }
+var checkSubarraySum = function (nums, k) {
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
     }
-    return i + 1;
+  }
+  return i + 1;
 };
 
 let nums = [1, 1, 2];
 let result = checkSubarraySum(nums);
 console.log("Expected Output: 2");
-
-
-
 
 // 02. Binary Search
 
@@ -32,31 +29,28 @@ console.log("Expected Output: 2");
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
 
-        if (nums[mid] === target) {
-            return mid; 
-        } else if (nums[mid] < target) {
-            left = mid + 1; 
-        } else {
-            right = mid - 1; 
-        }
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
+  }
 
-    return -1;
+  return -1;
 };
 
 let numsArr = [-1, 0, 3, 5, 9, 12];
 let targetVal = 9;
 console.log("Expected Output: 4");
-
-
-
 
 // 03. Search Insert Position
 
@@ -65,25 +59,55 @@ console.log("Expected Output: 4");
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
 
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
+  }
 
-    return left; 
+  return left;
 };
 
 let numsArr3 = [1, 3, 5, 6];
 let targetVal3 = 5;
 console.log("Expected Output: 2");
+
+
+
+
+// 04. Maximum Depth of Binary Tree
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0; 
+  let leftDepth = maxDepth(root.left); 
+  let rightDepth = maxDepth(root.right); 
+  return Math.max(leftDepth, rightDepth) + 1; 
+};
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+let root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+
+console.log("Expected Output: 3");
