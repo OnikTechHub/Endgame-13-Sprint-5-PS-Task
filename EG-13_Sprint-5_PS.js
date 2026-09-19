2f006ffd03e9695dad00c92637638930a7648d1c
onik-999 <onikdas.dev@gmail.com>
2026-09-19 13:37:04 +0600

    Add solution for problem 09: Continuous Subarray Sum

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -296,3 +296,41 @@ minStack.pop();
 let output2 = minStack.getMin(); 
 
 console.log("Expected Output: [-3, -2]");
+
+
+
+// 09. Continuous Subarray Sum
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    let map = new Map();
+    map.set(0, -1);
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        
+        if (k !== 0) {
+            sum %= k;
+        }
+
+        if (map.has(sum)) {
+            if (i - map.get(sum) >= 2) {
+                return true;
+            }
+        } else {
+            map.set(sum, i);
+        }
+    }
+    return false;
+};
+
+let nums9 = [23, 2, 4, 6, 7];
+let kVal9 = 6;
+
+console.log("Expected Output: true");
